ac70560c64a8037445a04645b8623c53679edf08
Andrew <andrew.madrigal23@gmail.com>
2026-01-15 14:04:58 -0800

    fixing merge conflicts

--- Linked Lists/LinkListInt.cpp
+++ Linked Lists/LinkListInt.cpp
@@ -2,6 +2,8 @@
 
 LinkListInt::LinkListInt()
 {
+    head = nullptr;
+    size = 0;
 }
 
 LinkListInt::~LinkListInt()
@@ -10,14 +12,42 @@ LinkListInt::~LinkListInt()
 
 void LinkListInt::Add(int value)
 {
+    Node* newNode = new Node();
+    newNode->value = value;
+    newNode->next = head;
+    head = newNode;
+    size++;
 }
 
 void LinkListInt::Remove(int value)
 {
+    Node* current = head;
+    Node* previous = nullptr;
+
+    while(current != nullptr) {
+        if(current->value == value) {
+            if(previous == nullptr) {
+                head = current->next;
+            } else {
+                previous->next = current->next;
+            }
+            delete current;
+            size--;
+            return;
+        }
+        previous = current;
+        current = current->next;
+    }
 }
 
 bool LinkListInt::Contains(int value)
 {
+    while(head != nullptr) {
+        if(head->value == value) {
+            return true;
+        }
+        head = head->next;
+    }
     return false;
 }
 
@@ -28,5 +58,12 @@ int LinkListInt::Size()
 
 string LinkListInt::ToString()
 {
-    return string();
+    string listStr = "";
+    Node* curr = head;
+    while (curr != nullptr) {
+        listStr += to_string(curr->value) + " ";
+        curr = curr->next;
+    }
+    listStr += "NULL";
+    return listStr;
 }
